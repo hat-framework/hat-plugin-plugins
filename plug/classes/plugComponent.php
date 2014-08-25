f1d004bd52af7753536d20287acbf5890e2540c5
Thom MF <tigredonorte3@gmail.com>
2014-08-25 09:49:54 -0300

    Design

--- plug/classes/plugComponent.php
+++ plug/classes/plugComponent.php
@@ -8,30 +8,25 @@ class plugComponent extends classes\Component\Component{
      );
      
      public function advanced($model, $item) {
-        $this->drawTitle($item);
-         $this->gui->opendiv('', 'span12');
-         $this->gui->closediv();
-         $opt = array('plugin' => $item['cod_plugin']);
-         $this->permissionWidget($opt);
+        //$this->drawTitle($item);
+         $this->permissionWidget($item);
          $this->gui->separator();
-         $this->modelWidget($opt);
+         $this->modelWidget($item);
      }
      
      
-     private function modelWidget($opt,$span = 'span6'){
-         $this->gui->opendiv('', $span);
-            \classes\Component\widget::executeWidgets(array(
-                'plugins/model/widgets/modelWidget' => $opt
-            ));
-         $this->gui->closediv();
+     private function modelWidget($item){
+         $opt = array('plugin' => $item['cod_plugin']);
+        \classes\Component\widget::executeWidgets(array(
+            'plugins/model/widgets/modelWidget' => $opt
+        ));
      }
      
-     private function permissionWidget($opt){
-         $this->gui->opendiv('', 'span6');
-            \classes\Component\widget::executeWidgets(array(
-                'plugins/permissao/widgets/permissionsWidget' => $opt
-            ));
-         $this->gui->closediv();
+     private function permissionWidget($item){
+         $opt = array('plugin' => $item['cod_plugin']);
+        \classes\Component\widget::executeWidgets(array(
+            'plugins/permissao/widgets/permissionsWidget' => $opt
+        ));
      }
      
      public function listar($model, $itens, $title = "", $class = '') {
@@ -63,8 +58,8 @@ class plugComponent extends classes\Component\Component{
          parent::DrawItem($model, $pkey, $item, $dados);
      }
      
-     private function getUrlImage($plugin){
-         $alternative_path = \classes\Classes\Registered::getTemplateLocation(CURRENT_TEMPLATE) . "/img/plugins_icon/{$plugin}_120.png";
+     private function getUrlImage($plugin, $size = '120'){
+         $alternative_path = \classes\Classes\Registered::getTemplateLocation(CURRENT_TEMPLATE) . "/img/plugins_icon/{$plugin}_$size.png";
          $relative_path = "/Config/img/img_120.png";
          if(file_exists(\classes\Classes\Registered::getTemplateLocation(CURRENT_TEMPLATE, true) .$alternative_path)) {
              $url = URL.$alternative_path;
@@ -100,4 +95,64 @@ class plugComponent extends classes\Component\Component{
          return (CURRENT_ACTION == "show")?$valor:"";
      }
      
+     public function showOne($item){
+        $img = $this->getUrlImage($item['plugnome'], '120');
+        $gui = new \classes\Component\GUI();
+        $gui->opendiv('', 'span12');
+            $gui->opendiv('', 'span3');
+                $gui->image($img, 'span12');
+            $gui->closediv();
+            $gui->opendiv('', 'span9 pull-right');
+                $gui->title($item['pluglabel']);
+                $gui->subtitle('R$ '.number_format($item['preco'], '2', ',', '.'));
+                $gui->subtitle('Versão: '. $item['versao']);
+                $gui->subtitle('Status: '. ucfirst($item['status']));
+                if(isset($item['detalhes']) && trim($item['detalhes']) !== ""){
+                    $gui->subtitle('Detalhes ');
+                    $gui->paragraph($item['detalhes']);
+                }
+            $gui->closediv();
+        $gui->closediv();
+        
+        $gui->opendiv('', 'span12');
+            $accd = $permw = $modlw = $exdata = "";
+            $this->tabs($item, $accd, $permw, $modlw, $exdata);
+            $tabs = array(
+                "Acessos diários" => $accd, 
+                "Permissões"      => $permw, 
+                "Modelos"         => $modlw, 
+                "Dados"           => $exdata, 
+            );
+            $this->LoadJsPlugin('jqueryui/tabs', 'jui')->draw($tabs);
+        $gui->closediv();
+     }
+     
+     private function tabs($item, &$accd, &$permw, &$modlw, &$exdata){
+        ob_start();
+        $this->acessosDiariosWidget($item);
+        $accd = ob_get_contents();
+        ob_end_clean();
+        
+        ob_start();
+        $this->permissionWidget($item);
+        $permw = ob_get_contents();
+        ob_end_clean();
+        
+        ob_start();
+        $this->modelWidget($item);
+        $modlw = ob_get_contents();
+        ob_end_clean();
+        
+        ob_start();
+        $this->show('plugins/plug', $item);
+        $exdata = ob_get_contents();
+        ob_end_clean();
+     }
+     
+     public function acessosDiariosWidget($item){
+        $opt = array('plugin' => $item['plugnome']);
+        classes\Component\widget::executeWidgets(array(
+            'plugins/plug/widgets/reportWidget' => $opt,
+        ));
+     }
 }
